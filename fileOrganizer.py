79a69a6ad7134dd5af843d18cda0e1d0ee91aed0
Amit Pallauri <apallauri@gmail.com>
2020-06-19 01:19:36 +0530

    modified

--- fileOrganizer.py
+++ fileOrganizer.py
@@ -3,13 +3,39 @@ import os
 import sys
 import shutil
 import ntpath
-import json
 
 
 # Dictionary to specify different types of extensions
-f = open('data.json')
-
-DIRECTORIES = json.load(f)
+DIRECTORIES = {
+    "HTML": [".html5", ".html", ".htm", ".xhtml"],
+    "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
+               ".heif", ".psd", ".jfif"
+               ],
+    "FILE": ["File"],
+    "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",
+               ".qt", ".mpg", ".mpeg", ".3gp"
+               ],
+    "DOCUMENTS": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",
+                  ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
+                  ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt",
+                  "pptx"
+                  ],
+    "ARCHIVES": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
+                 ".dmg", ".rar", ".xar", ".zip"
+                 ],
+    "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
+              ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"
+              ],
+    "PLAINTEXT": [".txt", ".in", ".out"],
+    "PDF": [".pdf"],
+    "PYTHON": [".py"],
+    "XML": [".xml"],
+    "EXE": [".exe"],
+    "SHELL": [".sh"],
+    "JS": ["javaScript File"],
+    "torrent": ["TORRENT File"],
+    "JSON": ["JSON File"]
+}
 FILE_FORMATS = {file_format: directory
                 for directory, file_formats in DIRECTORIES.items()
                 for file_format in file_formats}
